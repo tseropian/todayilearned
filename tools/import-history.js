c8970ab21ec67865d6c0c81309027e26dd122655
Thomas Seropian <thomas@seropian.io>
2021-07-16 14:08:50 +0100

    Tools for importing links from Firefox history

--- tools/import-history.js
+++ tools/import-history.js
@@ -4,44 +4,33 @@ const axios = require('axios');
 
 const db = new sqlite3.Database('/home/thomas/Desktop/places.sqlite');
 
-db.serialize(() => {
-  db.all('SELECT moz_historyvisits.visit_date, moz_places.url,	moz_places.title, moz_places.description FROM moz_historyvisits JOIN moz_places ON moz_historyvisits.place_id = moz_places.id ', (err, tables) => {
-    let cpt = 0;
-    for (const t of tables) {
-      if (t.url.includes('wikipedia.org')) {
-        cpt += 1;
-
-        const options = {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json;charset=UTF-8',
-          },
-          body: {
+(async () => {
+  db.serialize(async () => {
+    db.all('SELECT moz_historyvisits.visit_date, moz_places.url,	moz_places.title, moz_places.description FROM moz_historyvisits JOIN moz_places ON moz_historyvisits.place_id = moz_places.id ORDER BY visit_date ASC', (err, tables) => {
+      for (const t of tables) {
+        if (t.url.includes('wikipedia.org')) {
+          const options = {
             url: t.url,
-            date: t.visit_date,
+            date: Math.round(t.visit_date / 1000),
             source: 'Firefox History T480s',
-          },
-        };
-        // if (cpt < 3) {
-          // console.log(options);
-        const urlPost = 'https://bx57iqgdb5.execute-api.us-east-1.amazonaws.com/dev/links';
-        // const urlPost = 'http://localhost:3000/dev/links';
-        axios.post(
-          urlPost,
-          options,
-        )
-          .then((response) => {
-            console.log(response.data);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-        // }
-      }
-    }
-    // console.log(cpt);
-  });
-});
 
-db.close();
+          };
+          const urlPost = 'https://v2uvowrw9j.execute-api.eu-west-1.amazonaws.com/dev/links';
+          // const urlPost = 'http://localhost:3000/dev/links';
+          axios.post(
+            urlPost,
+            options,
+          )
+            .then((response) => {
+              console.log(response.data);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      }
+    });
+  });
+
+  db.close();
+})();
